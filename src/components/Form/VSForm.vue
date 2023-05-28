af08048949b5de62c974ceac1896d45241bd140e
khalil06kd <qweqwek617@gmail.com>
2023-05-28 22:57:03 +0800

    update vsform commit

---
+++ src/components/Form/VSForm.vue
@@ -0,0 +1,64 @@
+<template>
+    <form :class="['form', type]">
+      <slot></slot>
+    </form>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Form',
+    props: {
+      type: {
+        type: String,
+        default: 'primary'
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .form {
+    /* Custom form styles */
+    margin: 20px;
+    padding: 20px;
+    border-radius: 4px;
+    color: #fff;
+    cursor: pointer;
+  }
+  
+  /* Blue color palette */
+  .primary {
+    background-color: #007bff;
+    /* Default blue */
+  }
+  
+  .light {
+    background-color: #57abfa;
+    /* Light blue */
+  }
+  
+  .dark {
+    background-color: #192e42;
+    /* Dark blue */
+  }
+  
+  .blue1 {
+    background-color: #cce0ff;
+    /* Lighter blue */
+  }
+  
+  .blue2 {
+    background-color: #4d88ff;
+    /* Darker blue */
+  }
+  
+  /* Override button text color */
+  .primary,
+  .light,
+  .dark,
+  .blue1,
+  .blue2 {
+    color: #fff;
+  }
+  </style>
+  
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import VSButton from './Button/VSButton.vue';
 import VSCheckbox from './Checkbox/VSCheckbox.vue';
+import VSForm from './Form/VSForm.vue';
 
-export default { VSButton, VSCheckbox };
+export default { VSButton, VSCheckbox,VSForm };
