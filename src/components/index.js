431b00926f36d4139f065420626486d9f1a996ae
limmuelrabe <johnrabe055@gmail.com>
2023-05-28 18:35:22 +0800

    commit

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import VSButton from './Button/VSButton.vue';
 import VSSelect from './Select/VSSelect.vue';
 import VSRadioBox from './RadioBox/VSRadioBox.vue';
+import VSSwitch from './Switch/VSSwitch.vue';
 
-
-export default { VSButton, VSSelect, VSRadioBox};
+export default { VSButton, VSSelect, VSRadioBox, VSSwitch};
