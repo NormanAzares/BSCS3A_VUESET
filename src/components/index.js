f249efc1b730652ddfd40e7bf23da81351f58221
NovenXReyes <noven.reyes@gmail.com>
2023-05-28 18:24:49 +0800

    Add in index

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import VSButton from './Button/VSButton.vue';
 import VSSelect from './Select/VSSelect.vue';
+import VSRadioBox from './RadioBox/VSRadioBox.vue';
 
 
-export default { VSButton, VSSelect };
+export default { VSButton, VSSelect, VSRadioBox};
