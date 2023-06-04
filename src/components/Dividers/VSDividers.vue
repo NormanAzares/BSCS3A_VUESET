6b91c4a5d3e450b4c993f91223a0947b46041b5d
khalil06kd <qweqwek617@gmail.com>
2023-06-04 10:10:30 +0800

    new component - dividers

---
+++ src/components/Dividers/VSDividers.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="dividers">
+      <slot></slot>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Dividers'
+  }
+  </script>
+  
+  <style scoped>
+  .dividers {
+    /* Custom divider styles */
+    border-top: 1px solid #ccc;
+    margin: 10px 0;
+    padding: 10px 0;
+  }
+  </style>
