9646e7ac2bd1789ed1a41dd74b98dec869ace592
khalil06kd <qweqwek617@gmail.com>
2023-06-04 10:13:20 +0800

    import/export dividers component

--- src/components/index.js
+++ src/components/index.js
@@ -3,4 +3,5 @@ import VSSelect from './Select/VSSelect.vue';
 import VSRadioBox from './RadioBox/VSRadioBox.vue';
 import VSSwitch from './Switch/VSSwitch.vue';
 import VSForm from './Form/VSForm.vue';
-export default { VSButton, VSSelect, VSRadioBox, VSSwitch, VSForm};
+import VSDividers from './Dividers/VSDividers.vue';
+export default { VSButton, VSSelect, VSRadioBox, VSSwitch, VSForm, VSDividers};
