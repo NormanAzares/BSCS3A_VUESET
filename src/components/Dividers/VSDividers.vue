a227f3de13e93aec9950983ff9dead9e04d18084
khalil06kd <qweqwek617@gmail.com>
2023-06-04 10:21:40 +0800

    update  with diff color of blue and opacity

--- src/components/Dividers/VSDividers.vue
+++ src/components/Dividers/VSDividers.vue
@@ -1,20 +1,49 @@
+
 <template>
-    <div class="dividers">
-      <slot></slot>
-    </div>
+    <div :style="dividerStyle"></div>
   </template>
   
   <script>
   export default {
-    name: 'Dividers'
+    name: 'Dividers',
+    props: {
+      color: {
+        type: String,
+        default: 'primary'
+      }
+    },
+    computed: {
+      dividerStyle() {
+        return {
+          height: '1px',
+          backgroundColor: this.getColor(),
+          opacity: 1,
+          margin: '10px 0'
+        };
+      }
+    },
+    methods: {
+      getColor() {
+        switch (this.color) {
+          case 'primary':
+            return '#007bff'; /* Default blue */
+          case 'light':
+            return '#57abfa'; /* Light blue */
+          case 'dark':
+            return '#192e42'; /* Dark blue */
+          case 'blue1':
+            return '#cce0ff'; /* Lighter blue */
+          case 'blue2':
+            return '#4d88ff'; /* Darker blue */
+          default:
+            return '#007bff'; /* Default blue */
+        }
+      }
+    }
   }
   </script>
   
   <style scoped>
-  .dividers {
-    /* Custom divider styles */
-    border-top: 1px solid #ccc;
-    margin: 10px 0;
-    padding: 10px 0;
-  }
+  /* No additional styles needed */
   </style>
+  
