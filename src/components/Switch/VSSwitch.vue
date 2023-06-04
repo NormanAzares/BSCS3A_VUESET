3b3dc9b456a71617023f618e6e3e4eb73e15fbf3
limmuelrabe <johnrabe055@gmail.com>
2023-06-04 18:12:13 +0800

    update

--- src/components/Switch/VSSwitch.vue
+++ src/components/Switch/VSSwitch.vue
@@ -1,58 +1,157 @@
 <template>
-  <div class="switch" :class="{ on: value }" @click="toggleSwitch">
-    <div class="handle"></div>
-  </div>
+  <label
+    class="c-Switch"
+    v-bind="switchAttributes"
+    @keydown.space.prevent
+    @keyup.enter="onTrigger()"
+    @keyup.space="onTrigger()"
+  >
+    <input
+      v-model="state"
+      aria-hidden="true"
+      class="c-Switch__input"
+      type="checkbox"
+      :disabled="disabled"
+      @click="onTrigger()"
+    />
+    <div class="c-Switch__slider" />
+    <span v-if="label" class="c-Switch__label">
+      {{ label }}
+    </span>
+  </label>
 </template>
-
 <script>
 export default {
   name: 'Switch',
   props: {
-    value: {
+    modelValue: {
       type: Boolean,
-      default: false
+      required: false,
+      default: false,
+    },
+    label: {
+      type: String,
+      required: false,
+      default: undefined,
+    },
+    disabled: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+  emits: ['update:modelValue'],
+  data() {
+    return {
+      state: this.modelValue,
     }
+  },
+  computed: {
+    switchAttributes() {
+      return {
+        'aria-disabled': this.disabled,
+        tabindex: this.disabled ? undefined : '0',
+      }
+    },
+  },
+  watch: {
+    modelValue(newModelValue) {
+      this.state = newModelValue
+    },
+    state(newValue) {
+      this.$emit('update:modelValue', newValue)
+    },
   },
   methods: {
-    toggleSwitch() {
-      this.$emit('input', !this.value);
-      this.$nextTick(() => {
-        console.log(this.value);
-      });
-    }
+    onTrigger() {
+      this.state = !this.state
+    },
   },
-
 }
-
 </script>
-
 <style scoped>
-.switch {
-  width: 60px;
-  height: 30px;
-  background-color: #dcdcdc;
-  border-radius: 15px;
-  position: relative;
+.c-Switch {
   cursor: pointer;
-  transition: background-color 0.3s;
+  position: relative;
+  display: flex;
+  align-items: center;
+  width: -moz-fit-content;
+  width: fit-content;
 }
 
-.switch.on {
-  background-color: #4caf50;
+.c-Switch input {
+  display: none;
 }
 
-.handle {
-  width: 24px;
+.c-Switch__slider {
+  position: relative;
+  display: block;
+  width: 48px;
   height: 24px;
-  background-color: #fff;
-  border-radius: 50%;
-  position: absolute;
-  top: 3px;
-  left: 3px;
-  transition: left 0.3s;
+  border: 2px solid black;
+  border-radius: 24px;
+  background-color: white;
+  transition: 0.15s;
 }
 
-.switch.on .handle {
-  left: calc(100% - 27px);
+.c-Switch__slider:before {
+  content: '';
+  border-radius: 24px;
+  position: absolute;
+  position: absolute;
+  height: 16px;
+  width: 16px;
+  left: 2px;
+  top: 2px;
+  background-color: black;
+  transition: 0.15s;
+}
+
+.c-Switch__label {
+  user-select: none;
+  margin-left: 8px;
+}
+
+/* Checked */
+
+.c-Switch__input:checked + .c-Switch__slider {
+  border-color: #101010;
+  background-color: #101010;
+}
+
+.c-Switch__input:focus + .c-Switch__slider {
+  box-shadow: 0 0 1px #101010;
+}
+
+.c-Switch__input:checked + .c-Switch__slider:before {
+  background-color: white;
+  transform: translate(24px);
+}
+
+/* Disabled */
+
+.c-Switch[aria-disabled='true'] {
+  cursor: not-allowed;
+}
+
+.c-Switch[aria-disabled='true'] .c-Switch__slider {
+  border: 2px solid #757575;
+  background-color: #e0e0e0;
+}
+
+.c-Switch[aria-disabled='true'] .c-Switch__input:checked + .c-Switch__slider {
+  border: 2px solid #757575;
+  background-color: #757575;
+}
+
+.c-Switch[aria-disabled='true'] .c-Switch__slider:before {
+  background-color: #757575;
+}
+
+.c-Switch[aria-disabled='true']
+  .c-Switch__input:checked
+  + .c-Switch__slider:before {
+  background-color: #e0e0e0;
+  transform: translate(24px);
 }
 </style>
