ca104c0bdcdeb027e721e7191f510eaca132f813
BunaoCarlMatthew <130907061+BunaoCarlMatthew@users.noreply.github.com>
2023-06-11 18:44:45 +0800

    Updated Radiobox

--- src/components/RadioBox/VSRadioBox.vue
+++ src/components/RadioBox/VSRadioBox.vue
@@ -1,101 +1,141 @@
 <template>
-    <div>
-      <label :class="['radio', { 'radio--blue': isBlue }]">
-        <input type="radio" :name="name" :value="value" v-model="selectedValue" :disabled="isDisabled" />
-        <span class="checkmark"></span>
-        {{ label }}
-      </label>
-    </div>
+  <template v-for="(item, index) in items" :key="`radio-${index}`">
+    <label class="c-Radio" v-bind="radioAttributes" @keydown.space.prevent @keyup.enter="handleChange(item.value)"
+      @keyup.space="handleChange(item.value)">
+      <input v-model="selectedValue" aria-hidden="true" class="c-Radio__input" type="radio" :name="name"
+        :value="item.value" :disabled="disabled" />
+      <div class="c-Radio__element"></div>
+      <span v-if="item.label" class="c-Radio__label">
+        {{ item.label }}
+      </span>
+    </label>
   </template>
-  
-  <script>
-  export default {
-    name: 'VSRadioBox',
-    props: {
-      label: {
-        type: String,
-        required: true
+</template>
+<script>
+export default {
+  name: 'Radio',
+  props: {
+    modelValue: {
+      type: String,
+      required: false,
+      default: undefined,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    items: {
+      type: Array,
+      required: true,
+      validator: (items) => {
+        let isValid = true
+
+        items.forEach((item) => {
+          isValid = isValid && 'label' in item && 'value' in item
+        })
+
+        return isValid
       },
-      name: {
-        type: String,
-        required: true
-      },
-      value: {
-        type: String,
-        required: true
-      },
-      isDisabled: {
-        type: Boolean,
-        default: false
-      },
-      isBlue: {
-        type: Boolean,
-        default: false
+    },
+    disabled: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+  emits: ['update:modelValue'],
+  data() {
+    return {
+      selectedValue: this.modelValue,
+    }
+  },
+  computed: {
+    radioAttributes() {
+      return {
+        'aria-disabled': this.disabled,
+        tabindex: this.disabled ? undefined : '0',
       }
     },
-    data() {
-      return {
-        selectedValue: ''
-      };
-    }
-  };
-  </script>
-  
-  <style>
-  .radio {
-    position: relative;
-    display: inline-block;
-    margin-right: 10px;
-    cursor: pointer;
-    font-size: 16px;
-  }
-  
-  .radio--blue {
-    color: blue;
-  }
-  
-  .radio input {
-    position: absolute;
-    opacity: 0;
-    cursor: pointer;
-  }
-  
-  .checkmark {
-    position: absolute;
-    top: 0;
-    left: 0;
-    height: 16px;
-    width: 16px;
-    background-color: #eee;
-    border-radius: 50%;
-  }
-  
-  .radio--blue .checkmark {
-    background-color: blue;
-  }
-  
-  .radio input:checked ~ .checkmark {
-    background-color: #2196F3;
-  }
-  
-  .checkmark:after {
-    content: "";
-    position: absolute;
-    display: none;
-  }
-  
-  .radio input:checked ~ .checkmark:after {
-    display: block;
-  }
-  
-  .radio .checkmark:after {
-    left: 4px;
-    top: 4px;
-    width: 8px;
-    height: 8px;
-    border-radius: 50%;
-    background: white;
-  }
-  
-  </style>
-  
+  },
+  watch: {
+    modelValue(newModelValue) {
+      this.selectedValue = newModelValue
+    },
+    selectedValue(newValue) {
+      this.$emit('update:modelValue', newValue)
+    },
+  },
+  methods: {
+    handleChange(value) {
+      this.selectedValue = value
+    },
+  },
+}
+</script>
+<style scoped>
+.c-Radio {
+  cursor: pointer;
+  position: relative;
+  display: flex;
+  align-items: center;
+  width: -moz-fit-content;
+  width: fit-content;
+}
+
+.c-Radio+.c-Radio {
+  margin-top: 8px;
+}
+
+.c-Radio input {
+  display: none;
+}
+
+.c-Radio__element {
+  position: relative;
+  display: block;
+  width: 24px;
+  height: 24px;
+  border: 1px solid;
+  border-radius: 24px;
+  border-color: rgb(1, 0, 66);
+}
+
+.c-Radio__element::after {
+  content: '';
+  display: block;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 16px;
+  height: 16px;
+  border-radius: 16px;
+  background-color: rgb(56, 107, 184);
+  opacity: 0;
+  transition: 0.15s;
+}
+
+.c-Radio__input:checked+.c-Radio__element::after {
+  opacity: 1;
+}
+
+.c-Radio__label {
+  user-select: none;
+  margin-left: 8px;
+}
+
+/* Disabled */
+
+.c-Radio[aria-disabled='true'] {
+  cursor: not-allowed;
+}
+
+.c-Radio[aria-disabled='true'] .c-Radio__element {
+  border-color: #404c6e;
+  background-color: #e0e0e0;
+}
+
+.c-Radio[aria-disabled='true'] .c-Radio__element::after {
+  background-color: #565675;
+}
+</style>
